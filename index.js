f54547fffeb13f358b250ffc946fdfc4b36fc808
Shubh Johri (Torinit) <shubh.johri@torinit.ca>
2022-05-05 09:40:59 +0530

    fixed import/require issues

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const chalk = require("chalk")
-const clear = require("clear")
-const figlet = require("figlet")
+import chalk from 'chalk'
+import clear from  'clear'
+import figlet from 'figlet'
+import { directoryExists, getCurrentDirectoryBase } from "./lib/files.js"
 
-const files = require("./lib/files")
+
+clear();
+
+console.log(chalk.yellow(figlet.textSync('Ginit', {
+    horizontalLayout: 'full'
+})))
+
+if(directoryExists('.git')){
+    console.log(chalk.red('Already a git repository..!'))
+    process.exit()
+}
